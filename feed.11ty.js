9cd59f03bcef7c73c4970e54737f523c6dec7bca
jacob whitford-bender <jacob@xmentium.com>
2022-08-16 16:22:57 -0600

    fix: rss feed actually works lol

--- feed.11ty.js
+++ feed.11ty.js
@@ -13,36 +13,49 @@ const data = {
   }
 }
 
+const joinMap = f => xs => xs.map (f).join ('')
+
+const formatProps = o => {
+  const s = Object.entries (o ?? {}).map (([k, v]) => `${k}="${v}"`).join (' ')
+  return s.length ? ` ${s}` : ''
+}
+
+const createElement = tag => props => children => {
+  const formattedProps = formatProps (props)
+  const formattedChildren = Array.isArray (children) ? children.join ('')
+    : children.toString                              ? children.toString()
+    : ''
+  return `<${tag}${formattedProps}>${formattedChildren}</${tag}>`
+}
+
+const c = createElement
+
+const getDate = ({data: {title, updated, published, date}}) => 
+  updated || published || date ? (updated ?? published ?? date).toISOString () : ''
+
+const formatItem = ({data, url}) => c ('item') () ([
+  `<title>${data.title}</title>`,
+  `<link>daxi.ml${url}</link>`,
+  getDate ({data}) ? c ('updated') () (getDate ({data})) : ''
+])
 const render = props => {
   const {collections, metadata} = props
   const {post, posts} = collections
-  console.log('post', post);
-  return `
-    <?xml version="1.0" encoding="utf-8"?>
-    <feed xmlns="http://www.w3.org/2005/Atom">
-      <title>${metadata.title}</title>
-      <subtitle>${metadata.subtitle}</subtitle>
-      <link href="${metadata.feedUrl}" rel="self"/>
-      <link href="${metadata.url}"/>
-      <updated>${new Date ().toISOString ()}</updated>
-      <id>${metadata.url}</id>
-      <author>
-        <name>${metadata.author.name}</name>
-        <email>${metadata.author.email}</email>
-      </author>
-      ${post.map (post => `
-      <entry>
-        <title>${post.data.title}</title>
-        <link href="${post.url}"/>
-        ${post.updated || post.published
-          ? `<updated>${post.updated ?? post.published}</updated>`
-          : ''}
-        <id>${post.url}</id>
-        <content type="html">${post.templateContent}</content>
-      </entry>
-      `)}
-    </feed>
-  `.trim ()
+  const {title, subtitle, feedUrl, url, author} = metadata
+  const rss = c ('rss') ({version: '2.0'}) ([
+    c ('channel') () (`
+<title>${title}</title>
+<description>${subtitle}</description>
+<link>${url}</link>
+<lastBuildDate>${new Date ().toISOString ()}</lastBuildDate>
+<language>en-ca</language>
+<copyright>peer production license</copyright>
+<generator>ViperMADEthisBEAt</generator>
+<docs>https://www.rssboard.org/rss-specification</docs>
+${joinMap (formatItem) (post.sort ((p1, p2) => getDate(p1) > getDate(p2)))}
+    `)
+  ])
+  return `<?xml version="1.0" encoding="utf-8"?>${rss}`.trim ()
 }
 
 module.exports = {render, data}
