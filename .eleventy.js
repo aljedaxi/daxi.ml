0e624ea63989115e3cd3a98fd3d05414bee9eba8
jacob whitford-bender <jacob@xmentium.com>
2022-09-06 14:52:18 -0600

    feat: added books

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 const pluginRss = require ('@11ty/eleventy-plugin-rss')
 const markdownIt = require ('markdown-it')
+const events = require ('events')
+const fs = require ('node:fs')
+const readline = require ('node:readline')
 
 module.exports = eleventyConfig => {
 	eleventyConfig.addPlugin (pluginRss)
@@ -14,4 +17,20 @@ module.exports = eleventyConfig => {
 				markerPattern: /^\[\[tableofcontents\]\]/im,
 			})
 	)
+	eleventyConfig.addDataExtension('jsonl', {
+		parser(filename) {
+			const rl = readline.createInterface ({
+				input: fs.createReadStream (filename),
+				crlfDelay: Infinity,
+			})
+			return {
+				jsonl: async function* getLines() {
+					for await(const l of rl) {
+						yield JSON.parse (l)
+					}
+				}
+			}
+		},
+		read: false,
+	})
 }
