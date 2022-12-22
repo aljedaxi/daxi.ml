f66575f1b22f0eb8bb990c4cad517b207871b42a
jacob whitford-bender <jacob@xmentium.com>
2022-12-21 19:42:58 -0700

    feat: taster skeleton

---
+++ src/wine-taster/index.jsx
@@ -0,0 +1,13 @@
+export const layout = 'mainLayout.tmpl.js'
+export default props => {
+	return (
+		<body>
+			<main>
+				<h1>epic wine tasting companion</h1>
+				<h2>wine regions TODO</h2>
+				<h2>wine taste wheel TODO</h2>
+				<h2>wine color thing TODO</h2>
+			</main>
+		</body>
+	)
+}
